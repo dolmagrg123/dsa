e0cd7245c6008b7a456984b07d51cd6bf80e0d41
Dolma <dolmatgurung1@gmail.com>
2025-07-31 23:29:28 -0400

    Adding class structure to implement multi player features

--- generator.py
+++ generator.py
@@ -1,36 +1,44 @@
 import requests
 import json
+import os #Import os to secure our APIKEY as env variable
 
-API_KEY = "YOUR API KEY"
-url = "https://api.random.org/json-rpc/4/invoke"
+class RandomNumberGenerator:
+    def __init__(self):
+        self.API_KEY = os.environ.get("RANDOM_NUM_API_KEY") 
+        self.url = "https://api.random.org/json-rpc/4/invoke"
 
-
-def generate_random_integers():
-    payload = {
-        "jsonrpc": "2.0",
-        "method": "generateIntegers",
-        "params" : {
-            "apiKey": API_KEY,
-            "n": 4,
-            "min": 0,
-            "max": 7,
-            "base": 10,
-            "replacement": True,
-        },
-        "id":1
-    }
-    try:
-        response = requests.post(url,json=payload)
-        response.raise_for_status() # Raise an HTTPError for bad responses (4xx or 5xx)
-        response_json = response.json()
-
-        # print(response_json)
-        if "result" in response_json:
-            random_data = response_json["result"]["random"]["data"]
-            return random_data
-        else:
-            print("Error: 'result' key not found in the response.")
+        if not self.API_KEY:
+            print("API KEY had not been set in the OS")
             
-    except requests.exceptions.RequestException as e:
-        print(f"Error calling random.org API: {e}")
-        return None # Return None to indicate an error
+
+
+    def generate_random_integers(self):
+        payload = {
+            "jsonrpc": "2.0",
+            "method": "generateIntegers",
+            "params" : {
+                "apiKey": self.API_KEY,
+                "n": 4,
+                "min": 0,
+                "max": 7,
+                "base": 10,
+                "replacement": True,
+            },
+            "id":1
+        }
+        try:
+            response = requests.post(self.url,json=payload)
+            response.raise_for_status() # Raise an HTTPError for bad responses (4xx or 5xx)
+            response_json = response.json()
+
+            # print(response_json)
+            if "result" in response_json:
+                random_data = response_json["result"]["random"]["data"]
+                return random_data
+            else:
+                print("Error: 'result' key not found in the response.")
+                return None
+                
+        except requests.exceptions.RequestException as e:
+            print(f"Error calling random.org API: {e}")
+            return None # Return None to indicate an error
