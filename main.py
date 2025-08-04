0767a9b9398a46aa2bf6a61fc73a4ccb52023afd
Dolma <dolmatgurung1@gmail.com>
2025-08-04 00:51:13 -0400

    Changing the checker algorithm

--- main.py
+++ main.py
@@ -7,28 +7,34 @@ class Game:
 
     def __init__(self):
         self.level_manager = LevelManager()
-        num, min, max = self.level_manager.get_settings()
-        self.generator = RandomNumberGenerator(num,min,max)
-        self.user_input = UserInput(num,min,max)
+        self.generator = None
+        self.user_input = None
         self.remaining_guesses = 10
         self.target_combination  = None
 
-    def start_game(self):
 
-        self.target_combination  = self.generator.generate_random_integers()
+    def start_game(self):
+        num, min, max = self.level_manager.get_settings()
+
+        self.generator = RandomNumberGenerator(num,min,max)
+        self.target_combination  = self.generator.generate_random_integers() # random number generated
+        self.user_input = UserInput(num,min,max) # combination guessed by user
     
         for i in range(10):
             self.remaining_guesses -= 1
-
             user_guess = self.user_input.input_validator()
-            checker =GuessChecker(user_guess,self.target_combination)
+
+            checker = GuessChecker(user_guess,self.target_combination)
 
             if (checker.correct_combination()) == True:
                 break
-            else:
+            elif self.remaining_guesses > 0:
                 print (f"You have {self.remaining_guesses} guesses remaining")
+            else:
+                print (f"The correct combination is {self.target_combination }")
+                print("Sorry!!! You ran out of guesses. Please TRY AGAIN")
         
-        print (f"The correct combination is {self.target_combination }")
+        
         
 
 if __name__ == "__main__":
