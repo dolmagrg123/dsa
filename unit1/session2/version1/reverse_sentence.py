97dea9d00d26c0fd390b30e2fad13145aa3139a6
Dolma <dolmatgurung1@gmail.com>
2026-02-27 21:26:23 -0500

    unit1 problem1

---
+++ unit1/session2/version1/reverse_sentence.py
@@ -0,0 +1,50 @@
+#REVERSE SENTENCE
+
+'''
+UNDERSTAND
+
+1. Create a function reverse_sentence()
+2. Take string sentence as parameter
+3. return the sentence with words in reverse
+4. if only one word, return the original string
+
+** sentence -> alphabetic characters and spaces(to separate) only
+
+edge cases: what if it is empty string
+happy cases: reverse_sentence("hello friend") -> friend hello
+             reverse_sentence("hello") -> hello
+
+
+PLAN
+
+1. create function and take sentence string
+2. split the sentence and add to a list
+3. if len of list <=1 , return sentence
+4. create a new string
+5. create another list to add the words in reverse
+6. use .join() method to add the words from the second list into the string
+
+'''
+
+def reverse_sentence(sentence):
+
+    split_list = sentence.split()
+
+    if len(split_list) <= 1:
+        return sentence
+    
+    return_string = " "
+
+    reverse_list = split_list[::-1]
+
+    return_string = return_string.join(reverse_list)
+
+    return return_string
+
+sentence = "tubby little cubby all stuffed with fluff"
+print(reverse_sentence(sentence))
+
+sentence = "Pooh"
+print(reverse_sentence(sentence))
+
+
